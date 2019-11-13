1bef112e7f84e19459625a43dfea738ea3d16c72
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-13 16:25:37 +0200

    Adds theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,41 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+  prefix: '',
+  important: false,
+  separator: ':',
+  theme: {
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      primary: '#F34644',
+      darkAccent: '#998079',
+      darkShade: '#2C212E',
+      lightAccent: '#EBECE9',
+      lightShade: '#92928E',
+    },
+    fontFamily: {
+      body: [
+        'Lato',
+      ],
+    },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    }
+  },
+  corePlugins: {},
+  plugins: [],
 }
