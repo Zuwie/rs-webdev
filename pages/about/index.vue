8bc3a5125c26fa40f872614eaf593179ea14f5f9
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-13 16:27:15 +0200

    Adds about page

---
+++ pages/about/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="px-6 mx-auto">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga fugit
+      nam officiis praesentium quis tempore! Deserunt dolores ipsum ullam vero?
+      A aspernatur blanditiis consequuntur, culpa cum, cumque eum ex
+      exercitationem fuga illo ipsum maxime minima nostrum omnis placeat, quasi
+      quidem reprehenderit sapiente sed sint tempore tenetur velit vitae
+      voluptates!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga fugit
+      nam officiis praesentium quis tempore! Deserunt dolores ipsum ullam vero?
+      A aspernatur blanditiis consequuntur, culpa cum, cumque eum ex
+      exercitationem fuga illo ipsum maxime minima nostrum omnis placeat, quasi
+      quidem reprehenderit sapiente sed sint tempore tenetur velit vitae
+      voluptates!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga fugit
+      nam officiis praesentium quis tempore! Deserunt dolores ipsum ullam vero?
+      A aspernatur blanditiis consequuntur, culpa cum, cumque eum ex
+      exercitationem fuga illo ipsum maxime minima nostrum omnis placeat, quasi
+      quidem reprehenderit sapiente sed sint tempore tenetur velit vitae
+      voluptates!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga fugit
+      nam officiis praesentium quis tempore! Deserunt dolores ipsum ullam vero?
+      A aspernatur blanditiis consequuntur, culpa cum, cumque eum ex
+      exercitationem fuga illo ipsum maxime minima nostrum omnis placeat, quasi
+      quidem reprehenderit sapiente sed sint tempore tenetur velit vitae
+      voluptates!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga fugit
+      nam officiis praesentium quis tempore! Deserunt dolores ipsum ullam vero?
+      A aspernatur blanditiis consequuntur, culpa cum, cumque eum ex
+      exercitationem fuga illo ipsum maxime minima nostrum omnis placeat, quasi
+      quidem reprehenderit sapiente sed sint tempore tenetur velit vitae
+      voluptates!
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
