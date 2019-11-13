df42037cc28cf2f53d4df7e8726f5993e645ede5
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-13 16:26:53 +0200

    Basic header-styles

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,23 +1,8 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two" />
-    <div class="Triangle Triangle--one" />
-    <div class="Triangle Triangle--three" />
-    <div class="Triangle Triangle--four" />
-  </div>
+  <div class="rs-logo"></div>
 </template>
 
 <style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-
 .Triangle {
   position: absolute;
   top: 0;
