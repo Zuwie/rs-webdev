133124b4d3a5b2e2c8668d1a430bd27c315ac342
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-14 09:15:02 +0200

    Fixes flex layout, adjusts styling

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -3,9 +3,11 @@
     <div class="px-6 mx-auto max-w-5xl">
       <h1 class="site-title">{{ siteTitle }}</h1>
 
-      <div class="lg:flex -mx-4">
-        <div v-for="project in projects" class="w-1/3 px-4">
-          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="lg:flex lg:flex-wrap -mx-4">
+        <div v-for="project in projects" class="lg:w-1/3 px-4 mb-6">
+          <div
+            class="max-w-sm rounded overflow-hidden shadow-lg lg:h-full lg:flex lg:flex-col"
+          >
             <img
               class="w-full"
               src="https://via.placeholder.com/300x200"
@@ -15,10 +17,10 @@
               <div class="font-bold text-xl mb-2">{{ project.title }}</div>
               <p class="text-gray-700 text-base">{{ project.text }}</p>
             </div>
-            <div class="px-6 py-4">
+            <div class="px-6 py-4 lg:mt-auto">
               <span
                 v-for="tag in project.tags"
-                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+                class="inline-block bg-lightAccent rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
                 >#{{ tag }}</span
               >
             </div>
@@ -46,6 +48,30 @@ export default {
           text:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
           tags: ['Animation', 'Games', 'colorfull']
+        },
+        {
+          title: 'Bilfinger',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+          tags: ['Animation', 'Video']
+        },
+        {
+          title: 'HHLA',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+          tags: ['Animation', 'Video', 'Charts']
+        },
+        {
+          title: 'Shell',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+          tags: ['Accessibility']
+        },
+        {
+          title: 'Essity',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+          tags: ['Animation', 'Filter']
         }
       ]
     }
