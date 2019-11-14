89d0563a17e8da16dd099062fac7666e1cd6894b
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-14 10:52:23 +0200

    Move styles to default template

--- layouts/home.vue
+++ layouts/home.vue
@@ -20,22 +20,4 @@ export default {
 }
 </script>
 
-<style>
-html {
-  font-family: 'Lato', -apple-system, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-</style>
+<style></style>
