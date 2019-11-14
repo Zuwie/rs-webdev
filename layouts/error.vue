ad67b59aeceb813dd08e58735382758aeb60f7e1
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-14 10:51:25 +0200

    Adds default error-page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error-page p-12">
+    <h1 class="text-3xl">Oops, something went wrong!</h1>
+    <p>Back to <a href="/">safety</a>!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error'
+}
+</script>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
