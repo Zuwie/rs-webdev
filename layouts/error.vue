f349b526a1ee7bd6f820b98bd76642a285df1117
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-15 10:02:38 +0200

    Adds pictures and a mobile layout adjustments

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="error-page p-12">
+    <img src="~assets/404.png" alt="A man looking out for something" />
     <h1 class="text-3xl">Oops, something went wrong!</h1>
     <p>Back to <a href="/">safety</a>!</p>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         <div>
           <logo />
           <h1
-            class="font-bold text-darkShade text-4xl lg:text-6xl text-center mb-6"
+            class="font-bold text-darkShade text-4xl md:text-6xl text-center mb-6"
           >
             {{ siteTitle }}
           </h1>
@@ -28,6 +28,9 @@
           <li>Develop your personal Website using the latest technologies</li>
           <li>Web Apps shaped to your needs</li>
           <li>Consulting about the needs of your individual projects</li>
+          <li>
+            Optimize your site to get higher ranked at Google's Search-Engine
+          </li>
         </ul>
       </div>
     </section>
@@ -36,60 +39,80 @@
       <div class="p-6 py-12 mb-10 mx-auto max-w-5xl bg-white">
         <h2 class="chapter-title text-grey">Why me?</h2>
 
-        <div class="lg:flex lg:justify-between mt-12">
-          <div class="bg-white rounded p-6 shadow lg:w-2/3">
-            <h3 class="font-bold text-3xl mb-6">Argument</h3>
-            <p class="font-bold text-xl mb-4">
-              Lorem ipsum dolor amet whatever
-            </p>
-            <p>
-              Experience on lots of different projects with difficult
-              requirements Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit. Architecto aspernatur at blanditiis consectetur culpa, cum
-              dicta dolore dolorem ducimus esse incidunt laudantium perspiciatis
-              quisquam soluta sunt vero voluptates. Blanditiis, consectetur.
-            </p>
+        <div class="md:flex md:flex-row-reverse md:justify-between mt-12">
+          <div class="md:w-2/5">
+            <img
+              class="mb-2 md:mb-0"
+              src="~assets/entry.png"
+              alt="A person entering a portal"
+            />
           </div>
-          <div class="lg:w-1/3">
-            <img src="https://via.placeholder.com/450" alt="" />
-          </div>
-        </div>
-
-        <div class="lg:flex lg:justify-between mt-12 lg:flex-row-reverse">
-          <div class="bg-white rounded p-6 shadow lg:w-2/3">
-            <h3 class="font-bold text-3xl mb-6">Argument</h3>
-            <p class="font-bold text-xl mb-4">
-              Lorem ipsum dolor amet whatever
-            </p>
-            <p>
-              Good understanding of what the market wants Lorem ipsum dolor sit
-              amet, consectetur adipisicing elit. Architecto aspernatur at
-              blanditiis consectetur culpa, cum dicta dolore dolorem ducimus
-              esse incidunt laudantium perspiciatis quisquam soluta sunt vero
-              voluptates. Blanditiis, consectetur.
-            </p>
-          </div>
-          <div class="lg:w-1/3">
-            <img src="https://via.placeholder.com/450" alt="" />
+          <div class="md:w-3/5">
+            <div class="bg-white rounded p-6 shadow md:mr-4">
+              <h3 class="font-bold text-2xl mb-6">Argument</h3>
+              <p class="font-bold text-xl mb-4">
+                Lorem ipsum dolor amet whatever
+              </p>
+              <p>
+                Experience on lots of different projects with difficult
+                requirements Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Architecto aspernatur at blanditiis consectetur culpa, cum
+                dicta dolore dolorem ducimus esse incidunt laudantium
+                perspiciatis quisquam soluta sunt vero voluptates. Blanditiis,
+                consectetur.
+              </p>
+            </div>
           </div>
         </div>
 
-        <div class="lg:flex lg:justify-between mt-12">
-          <div class="bg-white rounded p-6 shadow lg:w-2/3">
-            <h3 class="font-bold text-3xl mb-6">Argument</h3>
-            <p class="font-bold text-xl mb-4">
-              Lorem ipsum dolor amet whatever
-            </p>
-            <p>
-              Experience with different technologies Lorem ipsum dolor sit amet,
-              consectetur adipisicing elit. Architecto aspernatur at blanditiis
-              consectetur culpa, cum dicta dolore dolorem ducimus esse incidunt
-              laudantium perspiciatis quisquam soluta sunt vero voluptates.
-              Blanditiis, consectetur.
-            </p>
+        <div class="md:flex md:justify-between mt-12">
+          <div class="md:w-2/5">
+            <img
+              class="mb-2 md:mb-0"
+              src="~assets/searching.png"
+              alt="A person searching"
+            />
           </div>
-          <div class="lg:w-1/3">
-            <img src="https://via.placeholder.com/450" alt="" />
+          <div class="md:w-3/5">
+            <div class="bg-white rounded p-6 shadow md:ml-4">
+              <h3 class="font-bold text-2xl mb-6">Argument</h3>
+              <p class="font-bold text-xl mb-4">
+                I will build you robust, scalable web-apps or websites so you
+                won't have to worry about the future.
+              </p>
+              <p>
+                Good understanding of what the market wants Lorem ipsum dolor
+                sit amet, consectetur adipisicing elit. Architecto aspernatur at
+                blanditiis consectetur culpa, cum dicta dolore dolorem ducimus
+                esse incidunt laudantium perspiciatis quisquam soluta sunt vero
+                voluptates. Blanditiis, consectetur.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="md:flex md:flex-row-reverse md:justify-between mt-12">
+          <div class="md:w-2/5">
+            <img
+              class="mb-2 md:mb-0"
+              src="~assets/order-completed.png"
+              alt="Order completed"
+            />
+          </div>
+          <div class="md:w-3/5">
+            <div class="bg-white rounded p-6 shadow md:mr-4">
+              <h3 class="font-bold text-2xl mb-6">Argument</h3>
+              <p class="font-bold text-xl mb-4">
+                Lorem ipsum dolor amet whatever
+              </p>
+              <p>
+                Experience with different technologies Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Architecto aspernatur at
+                blanditiis consectetur culpa, cum dicta dolore dolorem ducimus
+                esse incidunt laudantium perspiciatis quisquam soluta sunt vero
+                voluptates. Blanditiis, consectetur.
+              </p>
+            </div>
           </div>
         </div>
       </div>
