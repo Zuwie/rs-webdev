d719357b95c1cf48319289a1085d3c59a939d3a5
rafaelseifert <rafaelseifert93@gmail.com>
2019-11-18 19:05:44 +0200

    Styling change

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="border-t border-t-2 border-lightShade mt-10 p-6">
-    <div class="w-full bg-white lg:flex lg:justify-between">
+  <footer class="border-t border-t-2 bg-darkShade text-white mt-10 p-6">
+    <div class="w-full lg:flex lg:justify-between">
       <ul class="lg:flex text-sm">
         <li>
           <a class="block lg:inline-block mr-4 text-center" href="#Contact"
@@ -18,7 +18,7 @@
       <div
         class="copyright block lg:inline-block mr-4 mt-4 lg:mt-0 text-sm text-center"
       >
-        &copy; Copyright {{ dateYear }}, Rafael Seifert
+        &copy; {{ dateYear }} Rafael Seifert
       </div>
       <a href="https://icons8.com" class="hidden"></a>
     </div>
